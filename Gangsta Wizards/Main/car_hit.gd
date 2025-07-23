87a6b75bed24ce15ad8deb9083d4c2f9bb2b7fd2
DURCAN! <durcan.colin@gmail.com>
2025-07-22 21:26:02 -0400

    cars now send you flying lol

--- Gangsta Wizards/Main/car_hit.gd
+++ Gangsta Wizards/Main/car_hit.gd
@@ -1,8 +1,8 @@
 extends Area3D
 
 @onready var player = get_tree().get_first_node_in_group("Player")
-var initial_speed = 70
-var decay = 1
+var initial_speed = 60 
+var decay = 35 #Inital y component of launch, decays to simulate gravity
 var flying = false
 var car_direction
 var fly_direction
@@ -11,12 +11,12 @@ var fly_direction
 func _physics_process(delta):
 	if flying:
 		fly()
-		decay -= decay*delta
+		decay -= delta*25
 
 func _on_body_entered(body):
 	if body == player:
 		car_direction = -(global_transform.basis.x.normalized())
-		fly_direction = (3*car_direction + Vector3.UP).normalized()
+		#fly_direction = (3*car_direction + Vector3.UP).normalized()
 		
 		player.can_move = false
 		flying = true
@@ -24,7 +24,8 @@ func _on_body_entered(body):
 		await get_tree().create_timer(3).timeout
 		player.can_move = true
 		flying = false
+		decay = 35
 		
 func fly():
-	player.velocity = fly_direction * initial_speed * decay
+	player.velocity = (car_direction * initial_speed) + (Vector3.UP * decay)
 	player.move_and_slide()
