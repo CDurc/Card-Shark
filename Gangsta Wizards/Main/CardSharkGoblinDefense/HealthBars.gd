0677ee2a37ed84f70d1d7444afde9d97946aaec2
DURCAN! <durcan.colin@gmail.com>
2025-08-02 22:47:26 -0400

    helth

---
+++ Gangsta Wizards/Main/CardSharkGoblinDefense/HealthBars.gd
@@ -0,0 +1,21 @@
+extends Control
+
+@onready var label = $Healthbar
+@onready var camera = $"../../../Player/Head/Camera"
+@onready var target = $".."
+
+func _process(_delta):
+	var world_pos = target.global_transform.origin + Vector3.UP * 1.0
+	var screen_pos = camera.unproject_position(world_pos)
+
+	# Optionally hide if behind camera
+	if camera.is_position_behind(world_pos):
+		label.visible = false
+	else:
+		label.visible = true
+		label.position = screen_pos
+		label.position = screen_pos - (label.size * 0.5) * label.scale
+
+		var distance = camera.global_transform.origin.distance_to(target.global_transform.origin)
+		var scale_factor = clamp(1.0 / distance * 10.0, 0.5, 2.0)
+		label.scale = Vector2.ONE * scale_factor
